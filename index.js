eb200a5cbf2238d3715ddffd9edfc774cd2853d1
javonjax <javonj461@hotmail.com>
2024-07-27 02:59:35 -0400

    Minor updates.

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 const TICKETMASTER_API_KEY = process.env.TICKETMASTER_API_KEY;
 const TICKETMASTER_API_URL = 'https://app.ticketmaster.com/discovery/v2/events.json';
 
+
+app.use(cors());
+
 app.get('/api/events', async (request, response) => {
     try {
         const queryParams = new URLSearchParams({
             apikey: TICKETMASTER_API_KEY,
+            size: 200,
             ...request.query
         }).toString();
 
         const res = await fetch(`${TICKETMASTER_API_URL}?${queryParams}`);
-
+        
         if(!res.ok) {
             throw new Error(`Network response error: ${res.statusText}`);
         }
-
+        
         const events = await res.json();
         response.json(events);
     }
     catch(error) {
-        console.log('Error fetching data from Ticketmaster:', error);
+        console.log('Error fetching data from Ticketmaster:\n', error);
         response.status(500).json({ error: 'Internal server error.'});
     }
-})
+});
 
 
 app.listen(PORT, () => {
