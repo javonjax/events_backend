5e58e45180deba1b4b56ae0f18c8cc495e0c3662
javonjax <javonj461@hotmail.com>
2024-08-02 14:44:43 -0400

    Updated data and time formatting.

--- index.js
+++ index.js
@@ -4,70 +4,99 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 const TICKETMASTER_API_KEY = process.env.TICKETMASTER_API_KEY;
-const TICKETMASTER_API_URL = 'https://app.ticketmaster.com/discovery/v2/events.json';
+const TICKETMASTER_EVENTS_API_URL = 'https://app.ticketmaster.com/discovery/v2/events.json';
+const TICKETMASTER_SUGGEST_API_URL = 'https://app.ticketmaster.com/discovery/v2/suggest';
 
 
 app.use(cors());
 
+
+/* GET events
+
+ */
 app.get('/api/events', async (request, response) => {
     try {
+
         const queryParams = new URLSearchParams({
             apikey: TICKETMASTER_API_KEY,
             size: 200,
             ...request.query
         }).toString();
 
-        const res = await fetch(`${TICKETMASTER_API_URL}?${queryParams}`);
+        const res = await fetch(`${TICKETMASTER_EVENTS_API_URL}?${queryParams}`);
         
         if(!res.ok) {
-            throw new Error(`Network response error: ${res.statusText}`);
+            throw new Error(`${res.status}: ${res.statusText}`);
         }
         
         const data = await res.json();
+
         const events = data._embedded.events;
 
+        // Converts date from YYYY-MM-DD to Weekday, Month DD. 
         const formatDate = (dateString) => {
-            const date = new Date(dateString);
+            const [year, month, day] = dateString.split('-').map(Number);
+            const date = new Date(year, month - 1, day);
             const options = { month: 'short', day: 'numeric', weekday: 'short'}
-
+            
             return date.toLocaleDateString('en-US', options);
         };
         
+        // Converts time from 24hr to 12hr format.
+        const formatTime = (timeString) => {
+            const [hours24, mins] = timeString.split(':');
+            const period = hours24 >= 12 ? 'PM' : 'AM';
+            const hours = hours24 % 12 || 12;
+            const time = `${hours}:${mins} ${period}`;
+
+            return time;
+        };
+        
+        // Filter to remove objects that are missing data.
         const eventFilter = (event) => {
             return (
                 event.name &&
-                event.dates.start.dateTime &&
+                event.dates.start.localDate &&
+                event.dates.start.localTime &&
                 event.priceRanges &&
+                event._embedded &&
                 event._embedded.venues &&
                 event._embedded.venues[0].city &&
                 event._embedded.venues[0].state
             );
         };
 
+        // Retrieve necessary information about events.
         const eventDetails = events.filter(eventFilter)
-        .map(event => ({
-            name: event.name,
+        .map(event => {
+            console.log(event.dates.start.localDate);
 
-            date: event.dates.start.dateTime ? formatDate(event.dates.start.dateTime.split('T')[0])
-                                             : 'Dates unavailable.',
+            return (
+                    {
+                        name: event.name,
 
-            time: event.dates.start.dateTime ? event.dates.start.dateTime.split('T')[1].slice(0, -1) 
-                                             : 'Time unavailable.',
+                        date: event.dates.start.localDate ? formatDate(event.dates.start.localDate)
+                                                        : 'Dates unavailable.',
 
-            priceMin: event.priceRanges ? event.priceRanges[0].min + ' ' + event.priceRanges[0].currency 
-                                        : 'Price unavailable.',
+                        time: event.dates.start.localTime ? formatTime(event.dates.start.localTime)
+                                                        : 'Time unavailable.',
 
-            priceMax: event.priceRanges ? event.priceRanges[0].max + ' ' + event.priceRanges[0].currency
-                                        : 'Price unavailable.',
+                        priceMin: event.priceRanges ? event.priceRanges[0].min + ' ' + event.priceRanges[0].currency 
+                                                    : 'Price unavailable.',
 
-            location: event._embedded.venues && event._embedded.venues[0].city && event._embedded.venues[0].state ? event._embedded.venues[0].city.name + ', ' + event._embedded.venues[0].state.stateCode
-                                                                                        : 'Location unavailable.',
+                        priceMax: event.priceRanges ? event.priceRanges[0].max + ' ' + event.priceRanges[0].currency
+                                                    : 'Price unavailable.',
 
-            venue: event._embedded.venues ? event._embedded.venues[0].name
-                                          : 'Venue name unavailable.'
-        }))
-        console.log(data);
-        response.json(eventDetails);
+                        location: event._embedded.venues && event._embedded.venues[0].city && event._embedded.venues[0].state ? event._embedded.venues[0].city.name + ', ' + event._embedded.venues[0].state.stateCode
+                                                                                                    : 'Location unavailable.',
+
+                        venue: event._embedded.venues ? event._embedded.venues[0].name
+                                                    : 'Venue name unavailable.'
+                    }
+            )
+        });
+        
+        response.json(data);
     }
     catch(error) {
         console.log('Error fetching data from Ticketmaster:\n', error);
@@ -75,6 +104,29 @@ app.get('/api/events', async (request, response) => {
     }
 });
 
+
+app.get('/api/suggest', async (request, response) => {
+    try {
+        const query = request.query;
+        const queryString = new URLSearchParams(query).toString();
+        
+        // startEndDateTime[0]=2024-07-01T00:00:00Z&startEndDateTime[1]=2024-08-31T23:59:59Z
+        const queryParams = `apikey=${TICKETMASTER_API_KEY}&${queryString}`;
+        const res = await fetch(`${TICKETMASTER_SUGGEST_API_URL}?${queryParams}`);
+
+        if(!res.ok){
+            throw new Error(`${res.status}: ${res.statusText}`);
+        }
+
+        const data = await res.json();
+        console.log(data);
+        response.send(data);
+    }
+    catch(error) {
+        console.log('Error fetching data from Ticketmaster:\n', error);
+        response.status(500).json({ error: 'Internal server error.'});
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Backend API listening at port ${PORT}`);
